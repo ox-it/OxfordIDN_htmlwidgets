e42ae176fc3bc71be8239e59599c13796bb7afb0
martinjhnhadley <martinjohnhadley@gmail.com>
2016-08-30 13:32:35 +0100

    Fixed references to old repository name

--- charts/StackedBarCharts/ui.R
+++ charts/StackedBarCharts/ui.R
@@ -8,7 +8,7 @@ shinyUI(fluidPage(
     HTML(
       paste0(
     "<p>","This template shiny app visualises data published to figshare concerning the number of desktop items on University staff computers, the data is pulled directly from the ","<a href=https://figshare.com/articles/Investigation_into_the_number_of_desktop_items_on_University_staff_computers_broken_down_by_department_and_operating_system_/3425729>figshare repository</a>.","</>",
-    "<p>","A thorough overview of the script behind this visualisation is available on ","<a href=https://github.com/ox-it/LiveData_htmlwidgets/tree/master/charts/BarCharts>GitHub</a>,"," you can also see what code is responsible for which operation in this application by selecting \"Show Code\""
+    "<p>","A thorough overview of the script behind this visualisation is available on ","<a href=https://github.com/ox-it/OxfordIDN_htmlwidgets/tree/master/charts/BarCharts>GitHub</a>,"," you can also see what code is responsible for which operation in this application by selecting \"Show Code\""
       ))
   ),
   sidebarLayout(

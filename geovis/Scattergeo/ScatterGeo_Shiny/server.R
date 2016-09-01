be6c49048adbca0b3d74b5ffa2338645dd621643
martinjhnhadley <martinjohnhadley@gmail.com>
2016-09-01 16:59:42 +0100

    Recommit

--- geovis/Scattergeo/ScatterGeo_Shiny/server.R
+++
@@ -1,9 +0,0 @@
-library(shiny)
-library(leaflet)
-library(scales)
-
-shinyServer(function(input, output) {
-  source("data-processing.R", local = T)
-  source("visualisation-and-ui.R", local = T)
-  
-})
